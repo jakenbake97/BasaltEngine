feac1b1a03c96bac4b7beb03125ede9f71b1d9d3
Jake Pigg <jakenbake97@gmail.com>
2021-04-09 23:10:47 -0500

    Use premake to build project and move dll to exe path

--- Basalt/src/Basalt/EntryPoint.h
+++ Basalt/src/Basalt/EntryPoint.h
@@ -7,15 +7,15 @@ int main(int argc, char** argv)
 	auto* app = Basalt::CreateApplication();
 	
 	Basalt::Log::Init(app->GetAppName());
-	BE_LOG(Basalt::ELogger::Core, Basalt::ELogSeverity::Trace, "Initialized Logger");
-	BE_LOG(Basalt::ELogger::Core, Basalt::ELogSeverity::Info, "Initialized Logger");
-	BE_LOG(Basalt::ELogger::Core, Basalt::ELogSeverity::Warn, "Initialized Logger");
-	BE_LOG(Basalt::ELogger::Core, Basalt::ELogSeverity::Error, "Initialized Logger\n");
+	BE_LOG(Basalt::ELogger::Core, Basalt::ELogSeverity::Trace, "Initialized Logger Trace");
+	BE_LOG(Basalt::ELogger::Core, Basalt::ELogSeverity::Info, "Initialized Logger Info");
+	BE_LOG(Basalt::ELogger::Core, Basalt::ELogSeverity::Warn, "Initialized Logger Warn");
+	BE_LOG(Basalt::ELogger::Core, Basalt::ELogSeverity::Error, "Initialized Logger Error\n");
 	
-	BE_LOG(Basalt::ELogger::Client, Basalt::ELogSeverity::Trace, "Initialized Logger");
-	BE_LOG(Basalt::ELogger::Client, Basalt::ELogSeverity::Info, "Initialized Logger");
-	BE_LOG(Basalt::ELogger::Client, Basalt::ELogSeverity::Warn, "Initialized Logger");
-	BE_LOG(Basalt::ELogger::Client, Basalt::ELogSeverity::Error, "Initialized Logger");
+	BE_LOG(Basalt::ELogger::Client, Basalt::ELogSeverity::Trace, "Initialized Logger Trace");
+	BE_LOG(Basalt::ELogger::Client, Basalt::ELogSeverity::Info, "Initialized Logger Info");
+	BE_LOG(Basalt::ELogger::Client, Basalt::ELogSeverity::Warn, "Initialized Logger Warn");
+	BE_LOG(Basalt::ELogger::Client, Basalt::ELogSeverity::Error, "Initialized Logger Error");
 	
 	app->Run();
 	delete app;
---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "BasaltEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Basalt"
+	location "Basalt"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("bin/" .. outputDir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputDir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BE_PLATFORM_WINDOWS",
+			"BE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputDir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "BE_DEBUG"
+		symbols "On"
+		buildoptions "/MDd"
+		
+	filter "configurations:Release"
+		defines "BE_RELEASE"
+		optimize "On"
+		
+	filter "configurations:Dist"
+		defines "BE_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" .. outputDir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputDir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Basalt/vendor/spdlog/include",
+		"Basalt/src"
+	}
+
+	links
+	{
+		"Basalt"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "BE_DEBUG"
+		symbols "On"
+		buildoptions "/MDd"
+		
+	filter "configurations:Release"
+		defines "BE_RELEASE"
+		optimize "On"
+		
+	filter "configurations:Dist"
+		defines "BE_DIST"
+		optimize "On"
